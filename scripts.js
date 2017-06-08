5f1ac8aedc6b2a03e4b51b2bba4646bb0c70805d
Malgorzata Groves <bromba1@tlen.pl>
2017-06-08 19:24:48 +0200

    new files for task 8.6

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+//tytul pliku
+var a = 2, b = 3, Value = (a * a) + (2 * a * b) - (b * b);
+if (Value > 0) {
+	console.log ('wartosc dodatnia');
+} else if (Value < 0) {
+	console.log ('wartosc ujemna)');
+} else  if (Value === 0) {console.log ('wartosc rowna zero');
+}
